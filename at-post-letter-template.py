c4a0f7a9c6be4c0b5a277021fb5afba6f332e472
roboes <rg.roberto@outlook.com>
2023-07-25 20:30:15 +0200

    Update

--- at-post-letter-template.py
+++ at-post-letter-template.py
@@ -1,8 +1,12 @@
 ## Post AG - Vorlage mit Absender Letter Template using ReportLab
-# Last update: 2023-05-25
+# Last update: 2023-07-19
 
 
-# Template: https://www.einfach-brief.at/fe/vorlagen
+"""
+About: Fill variables (of a given dataset input) into the Austrian Post AG - Vorlage mit Absender letter template using ReportLab library in Python.
+
+Template: https://www.einfach-brief.at/fe/vorlagen
+"""
 
 
 ###############
@@ -30,25 +34,52 @@ from reportlab.platypus import Paragraph, KeepInFrame, Table, TableStyle
 
 
 # Set working directory
-os.chdir(path=os.path.join(os.path.expanduser('~'), 'Downloads'))
-
-
+# os.chdir(path=os.path.join(os.path.expanduser('~'), 'Downloads'))
 
 
 ###########
 # Functions
 ###########
 
-# Create table frame - All flowables have an hAlign property: ('LEFT', 'RIGHT', 'CENTER' or 'CENTRE'). For paragraphs, which fill the full width of the frame, this has no effect.
-def create_table_frame(*, document, text, frameWidth, frameHeight, frameHPosition, frameVPosition, fontName='Arial', fontSize=11, textColor='black', textHAlign=TA_LEFT, textVAlign='TOP', textLeftPadding=0, textRightPadding=0, textTopPadding=0, textBottomPadding=0, showBoundary=True):
 
+def create_table_frame(
+    *,
+    document,
+    text,
+    frameWidth,
+    frameHeight,
+    frameHPosition,
+    frameVPosition,
+    fontName='Arial',
+    fontSize=11,
+    textColor='black',
+    textHAlign=TA_LEFT,
+    textVAlign='TOP',
+    textLeftPadding=0,
+    textRightPadding=0,
+    textTopPadding=0,
+    textBottomPadding=0,
+    showBoundary=True,
+):
+    """All flowables have an hAlign property: ('LEFT', 'RIGHT', 'CENTER' or 'CENTRE'). For paragraphs, which fill the full width of the frame, this has no effect."""
     ## Text
 
     # Add HTML paragraph breaks
     text = re.sub(pattern=r'\n', repl='<br/>', string=text)
 
     # Create ReportLab Paragraph object and apply font styles to text
-    text = Paragraph(text, ParagraphStyle(name='', fontName=fontName, fontSize=fontSize, leading=(12 if fontSize >= 10 else 9), textColor=textColor, alignment=textHAlign), encoding='utf8')
+    text = Paragraph(
+        text,
+        ParagraphStyle(
+            name='',
+            fontName=fontName,
+            fontSize=fontSize,
+            leading=(12 if fontSize >= 10 else 9),
+            textColor=textColor,
+            alignment=textHAlign,
+        ),
+        encoding='utf8',
+    )
 
     # In case of long text: shrink to fit inside table frame
     text = KeepInFrame(
@@ -56,11 +87,10 @@ def create_table_frame(*, document, text, frameWidth, frameHeight, frameHPositio
         maxHeight=0,
         content=[text],
         mode='shrink',
-        #hAlign=textHAlign,
-        #vAlign=textVAlign,
-        #fakeWidth=False,
+        # hAlign=textHAlign,
+        # vAlign=textVAlign,
+        # fakeWidth=False,
     )
-
 
     ## Table
 
@@ -71,36 +101,44 @@ def create_table_frame(*, document, text, frameWidth, frameHeight, frameHPositio
         rowHeights=frameHeight,
         spaceBefore=0,
         spaceAfter=0,
-        #hAlign=textHAlign,
-        #vAlign=textVAlign,
+        # hAlign=textHAlign,
+        # vAlign=textVAlign,
     )
 
     # Set table style
-    table.setStyle(TableStyle([
-        ('LEFTPADDING', (0, 0), (-1, -1), textLeftPadding),
-        ('RIGHTPADDING', (0, 0), (-1, -1), textRightPadding),
-        ('TOPPADDING', (0, 0), (-1, -1), textTopPadding),
-        ('BOTTOMPADDING', (0, 0), (-1, -1), textBottomPadding),
-        ('VALIGN', (0, 0), (-1, -1), textVAlign),
-    ]))
+    table.setStyle(
+        TableStyle(
+            [
+                ('LEFTPADDING', (0, 0), (-1, -1), textLeftPadding),
+                ('RIGHTPADDING', (0, 0), (-1, -1), textRightPadding),
+                ('TOPPADDING', (0, 0), (-1, -1), textTopPadding),
+                ('BOTTOMPADDING', (0, 0), (-1, -1), textBottomPadding),
+                ('VALIGN', (0, 0), (-1, -1), textVAlign),
+            ],
+        ),
+    )
 
     # Horizontal alignment
-    #if textHAlign == TA_CENTER:
-    #    table.setStyle(TableStyle([
-    #        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-    #    ]))
-    #if textHAlign == TA_LEFT or textHAlign == TA_JUSTIFY:
-    #    table.setStyle(TableStyle([
-    #        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
-    #    ]))
-    #else:
-    #    pass
+    # if textHAlign == TA_CENTER:
+    #     table.setStyle(TableStyle([
+    #         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    #     ]))
+    # if textHAlign == TA_LEFT or textHAlign == TA_JUSTIFY:
+    #     table.setStyle(TableStyle([
+    #         ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+    #     ]))
+    # else:
+    #     pass
 
     # Show boundary
-    if showBoundary == True:
-        table.setStyle(TableStyle([
-            ('GRID', (0, 0), (-1, -1), 0.5, 'black'),
-        ]))
+    if showBoundary is True:
+        table.setStyle(
+            TableStyle(
+                [
+                    ('GRID', (0, 0), (-1, -1), 0.5, 'black'),
+                ],
+            ),
+        )
 
     else:
         pass
@@ -108,21 +146,30 @@ def create_table_frame(*, document, text, frameWidth, frameHeight, frameHPositio
     table.wrap(0, 0)
 
     # Table location
-    table.drawOn(document, frameHPosition, (A4[1]-frameVPosition-frameHeight))
+    table.drawOn(document, frameHPosition, (A4[1] - frameVPosition - frameHeight))
 
 
-
-# Create document
-def create_document(*, df, title, author='', file_name='Output.pdf', subject=''):
-
+def create_document(
+    *,
+    df,
+    title,
+    author='',
+    directory,
+    file_name='Output.pdf',
+    subject='',
+):
     # Create document
-    document = Canvas(filename=file_name, pagesize=A4, bottomup=1, pdfVersion=(1,4))
+    document = Canvas(
+        filename=os.path.join(directory, file_name),
+        pagesize=A4,
+        bottomup=1,
+        pdfVersion=(1, 4),
+    )
     document.setAuthor(author)
     document.setSubject(subject)
     document.setTitle(title)
     document.setCreator('')
     document.setProducer('')
-
 
     # Load Arial font
     pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
@@ -130,18 +177,22 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
     pdfmetrics.registerFont(TTFont('Arial-Oblique', 'Ariali.ttf'))
     pdfmetrics.registerFont(TTFont('Arial-BoldOblique', 'Arialbi.ttf'))
 
-    pdfmetrics.registerFontFamily('Arial', normal='Arial', bold='Arial-Bold', italic='Arial-Oblique', boldItalic='Arial-BoldOblique')
-
+    pdfmetrics.registerFontFamily(
+        'Arial',
+        normal='Arial',
+        bold='Arial-Bold',
+        italic='Arial-Oblique',
+        boldItalic='Arial-BoldOblique',
+    )
 
     for index, row in df.iterrows():
-
         ### Document contents
 
         ## Title
 
         # Text
-        text = ("""Max Mustermann
-        Musterstr. 123 · 12345 Musterort""")
+        text = """Max Mustermann
+        Musterstr. 123 · 12345 Musterort"""
 
         # Create table frame
         create_table_frame(
@@ -151,18 +202,17 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
             fontSize=12,
             textHAlign=TA_CENTER,
             textVAlign='MIDDLE',
-            frameWidth=16*cm,
-            frameHeight=1.54*cm,
-            frameHPosition=2.5*cm,
-            frameVPosition=1.9*cm,
+            frameWidth=16 * cm,
+            frameHeight=1.54 * cm,
+            frameHPosition=2.5 * cm,
+            frameVPosition=1.9 * cm,
             showBoundary=False,
         )
-
 
         ## Adressfeld Absender/Einschreiben
 
         # Text
-        text = ("""Max Mustermann, Musterstr. 123, 12345 Musterort <i>(optional)</i>""")
+        text = """Max Mustermann, Musterstr. 123, 12345 Musterort <i>(optional)</i>"""
 
         # Create table frame
         create_table_frame(
@@ -172,29 +222,31 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
             fontSize=8,
             textHAlign=TA_LEFT,
             textVAlign='BOTTOM',
-            textLeftPadding=0.5*cm,
-            textRightPadding=0.1*cm,
-            textTopPadding=0.3*cm,
-            textBottomPadding=0.1*cm,
-            frameWidth=9*cm,
-            frameHeight=1.5*cm,
-            frameHPosition=2*cm,
-            frameVPosition=4.7*cm,
+            textLeftPadding=0.5 * cm,
+            textRightPadding=0.1 * cm,
+            textTopPadding=0.3 * cm,
+            textBottomPadding=0.1 * cm,
+            frameWidth=9 * cm,
+            frameHeight=1.5 * cm,
+            frameHPosition=2 * cm,
+            frameVPosition=4.7 * cm,
             showBoundary=False,
         )
-
 
         ## Adressfeld Adressblock
 
         # Text
-        text = ("""{gender}{name}
+        text = (
+            """{gender}{name}
         {street}
         {postal_code} {city}
         {country}"""
         ).format(
             name=row['name'],
-            gender='Frau ' if row['gender'] == 'F' else('Herr ' if row['gender'] == 'M' else ''),
-            country=row['location_country'],
+            gender='Frau '
+            if row['gender'] == 'F'
+            else ('Herr ' if row['gender'] == 'M' else ''),
+            country=row['location_country'].upper(),
             postal_code=row['location_postal_code'],
             city=row['location_city'],
             street=row['location_street'],
@@ -208,17 +260,16 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
             fontSize=11,
             textHAlign=TA_LEFT,
             textVAlign='MIDDLE',
-            textLeftPadding=0.5*cm,
-            textRightPadding=2.5*cm, # Post Matrixcode Frankierung margin
-            textTopPadding=0.1*cm,
-            textBottomPadding=0.1*cm,
-            frameWidth=9*cm,
-            frameHeight=3.1*cm,
-            frameHPosition=2*cm,
-            frameVPosition=6.2*cm,
+            textLeftPadding=0.5 * cm,
+            textRightPadding=2.5 * cm,  # Post Matrixcode Frankierung margin
+            textTopPadding=0.1 * cm,
+            textBottomPadding=0.1 * cm,
+            frameWidth=9 * cm,
+            frameHeight=3.1 * cm,
+            frameHPosition=2 * cm,
+            frameVPosition=6.2 * cm,
             showBoundary=False,
         )
-
 
         ## Date
 
@@ -230,7 +281,7 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
         text = ("""{city}{date}""").format(
             city=city + ', ' if city != '' else '',
             # date=date.strftime('%d. %B %Y'),
-            date=format_date(date, format='dd. MMM yyyy', locale='de_DE')
+            date=format_date(date, format='dd. MMM yyyy', locale='de_DE'),
         )
 
         # Create table frame
@@ -241,18 +292,17 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
             fontSize=11,
             textHAlign=TA_RIGHT,
             textVAlign='TOP',
-            frameWidth=6*cm,
-            frameHeight=0.5*cm,
-            frameHPosition=A4[0]-2.5*cm-6*cm,
-            frameVPosition=9.8*cm,
+            frameWidth=6 * cm,
+            frameHeight=0.5 * cm,
+            frameHPosition=A4[0] - 2.5 * cm - 6 * cm,
+            frameVPosition=9.8 * cm,
             showBoundary=False,
         )
-
 
         ## Inhalt
 
         # Text
-        text = ("""<b>Betreff: Briefvorlage Standard mit Absender</b>
+        text = """<b>Betreff: Briefvorlage Standard mit Absender</b>
 
 
         Sehr geehrte*r Anwender*in der Tages-Post,
@@ -271,7 +321,7 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
 
         Mit freundlichen Grüßen
         Ihr Tages-Post Team
-        """)
+        """
 
         # Create table frame
         create_table_frame(
@@ -281,20 +331,17 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
             fontSize=11,
             textHAlign=TA_JUSTIFY,
             textVAlign='TOP',
-            frameWidth=16*cm,
-            frameHeight=16.95*cm,
-            frameHPosition=2.5*cm,
-            frameVPosition=10.75*cm,
+            frameWidth=16 * cm,
+            frameHeight=16.95 * cm,
+            frameHPosition=2.5 * cm,
+            frameVPosition=10.75 * cm,
             showBoundary=False,
         )
 
         document.showPage()
 
-
     # Save document
     document.save()
-
-
 
 
 #########################
@@ -302,11 +349,54 @@ def create_document(*, df, title, author='', file_name='Output.pdf', subject='')
 #########################
 
 # Create example DataFrame with names, gender and addresses
-df = pd.DataFrame(data=[
-    ['Eva Muster', 'F', 'Österreich', 'Musterbundesland', '9875', 'Musterstadt', 'Musterstraße 1'],
-    ['Max Mustermann', 'M', 'Österreich', 'Musterbundesland', '9875', 'Musterstadt', 'Musterstraße 1'],
-    ['Firma ABC', '', 'Österreich', 'Musterbundesland', '9875', 'Industriestadt', 'Industriestraße 1'],
-    ], index=None, columns=['name', 'gender', 'location_country', 'location_state', 'location_postal_code', 'location_city', 'location_street'], dtype=None)
+df = pd.DataFrame(
+    data=[
+        [
+            'Eva Muster',
+            'F',
+            'Österreich',
+            'Musterbundesland',
+            '9875',
+            'Musterstadt',
+            'Musterstraße 1',
+        ],
+        [
+            'Max Mustermann',
+            'M',
+            'Österreich',
+            'Musterbundesland',
+            '9875',
+            'Musterstadt',
+            'Musterstraße 1',
+        ],
+        [
+            'Firma ABC',
+            '',
+            'Österreich',
+            'Musterbundesland',
+            '9875',
+            'Industriestadt',
+            'Industriestraße 1',
+        ],
+    ],
+    index=None,
+    columns=[
+        'name',
+        'gender',
+        'location_country',
+        'location_state',
+        'location_postal_code',
+        'location_city',
+        'location_street',
+    ],
+    dtype=None,
+)
 
-# Create PDF document
-create_document(df=df, title='Post AG - Vorlage mit Absender', author='Post AG', file_name='Post AG - Vorlage mit Absender.pdf')
+# Create .pdf document
+create_document(
+    df=df,
+    title='Post AG - Vorlage mit Absender',
+    author='Post AG',
+    directory=os.path.join(os.path.expanduser('~'), 'Downloads'),
+    file_name='Post AG - Vorlage mit Absender.pdf',
+)
